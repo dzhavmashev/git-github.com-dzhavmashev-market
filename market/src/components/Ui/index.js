f0187dedd1a16138daf28537e24538994e70e95f
d.djavmashev <d.djavmashev@mbulak.kg>
2023-03-30 20:01:49 +0600

    modified:   market/src/components/Ui/MyButton.vue 	modified:   market/src/components/Ui/index.js

--- market/src/components/Ui/index.js
+++ market/src/components/Ui/index.js
@@ -1,11 +1,9 @@
 import MyButton from './MyButton.vue'
 import LinkButton from './LinkButton.vue'
 import ModalWindow from './ModalWindow.vue'
-import MyInput from './MyInput.vue'
 
 export default [
     MyButton,
     LinkButton,
-    ModalWindow,
-    MyInput
+    ModalWindow
 ]
