175929a75033b2bc27485b7a5716117b246d729e
Temirlan <tema_baike@gmail.com>
2023-04-04 20:48:26 +0600

    modified:   market/package-lock.json 	modified:   market/package.json 	modified:   market/src/components/UI/indexfile.js 	deleted:    market/src/components/Ui/MyButton.vue

--- market/src/components/UI/indexfile.js
+++ market/src/components/UI/indexfile.js
@@ -3,6 +3,6 @@ import LinkButton from './LinkButton.vue'
 import ModalWindow from './ModalWindow.vue'
 import CustomButton from './CustomButton.vue'
 import CustomModal from './CustomModal.vue'
-import MyButton from './MyButton.vue'
 
-export default [LinkButton, ModalWindow, CustomButton, CustomModal, MyButton]
+
+export default [LinkButton, ModalWindow, CustomButton, CustomModal]
